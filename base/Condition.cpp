8a22b07859085087e93887d8fd42d0a4ec946bdf
iakud <kaikai87111@gmail.com>
2014-10-06 21:09:19 +0800

    fix 32bit

--- base/Condition.cpp
+++ base/Condition.cpp
@@ -6,7 +6,7 @@ bool Condition::timedwait(uint64_t timeout) {
 	struct timespec abstime;
 	::clock_gettime(CLOCK_REALTIME, &abstime);
 	timeout = timeout + abstime.tv_sec * kNanoSecondsPerSecond + abstime.tv_nsec;
-	abstime.tv_sec = timeout / kNanoSecondsPerSecond;
-	abstime.tv_nsec = timeout % kNanoSecondsPerSecond;
+	abstime.tv_sec = static_cast<__time_t>(timeout / kNanoSecondsPerSecond);
+	abstime.tv_nsec = static_cast<long int>(timeout % kNanoSecondsPerSecond);
 	return !(::pthread_cond_timedwait(&cond_, &mutex_.getPthreadMutex(), &abstime));
 }
