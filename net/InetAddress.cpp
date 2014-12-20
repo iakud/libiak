7564a0ca80bdb28d735f504fc7cf91524f07ca2d
iakud <kaikai87111@gmail.com>
2014-12-20 16:19:05 +0800

    bzero

--- net/InetAddress.cpp
+++ net/InetAddress.cpp
@@ -1,8 +1,11 @@
 #include "InetAddress.h"
 
+#include <string.h>
+
 using namespace iak;
 
 InetAddress::InetAddress(uint16_t port, const char* ip) {
+	::bzero(&addr_, sizeof(addr_));
 	addr_.sin_family = AF_INET;
 	addr_.sin_port = htons(port);
 	::inet_pton(AF_INET, ip, &addr_.sin_addr);
