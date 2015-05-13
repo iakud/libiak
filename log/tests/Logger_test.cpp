9965543702826c55432d442b45a298802df850c0
iakud <kaikai87111@gmail.com>
2015-05-13 17:52:54 +0800

    update

--- log/tests/Logger_test.cpp
+++ log/tests/Logger_test.cpp
@@ -9,8 +9,7 @@ LoggerPtr g_loggerPtr;
 int main(int argc, char* argv[]) {
 	char name[256];
 	strncpy(name, argv[0], 256);
-	g_loggerPtr = Logger::make(::basename(name), 200 * 1000);
-
+	g_loggerPtr = Logger::make(::basename(name), 200 * 1000, false, 3, true, true);
 
 	std::string line = "1234567890 abcdefg FDAGFSADGASDF   ";
 	for (int i=0; i<1000; ++i) {
