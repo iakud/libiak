dec76fe2e62b22a61918a09487f5a003839d1b53
iakud <kaikai87111@gmail.com>
2015-09-05 18:32:29 +0800

    change connection

---
+++ net/Socket.cpp
@@ -0,0 +1,75 @@
+#include "Socket.h"
+
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+using namespace iak::net;
+
+int Socket::open() {
+	return ::socket(AF_INET, SOCK_STREAM|SOCK_NONBLOCK|SOCK_CLOEXEC, IPPROTO_TCP);
+}
+
+int Socket::close(int sockFd) {
+	return ::close(sockFd);
+}
+
+int Socket::bind(int sockFd, const struct sockaddr_in &addr) {
+	return ::bind(sockFd, reinterpret_cast<const struct sockaddr*>(&addr), static_cast<socklen_t>(sizeof addr));
+}
+
+int Socket::listen(int sockFd) {
+	return ::listen(sockFd, SOMAXCONN);
+}
+
+int Socket::accept(int sockFd, struct sockaddr_in *addr) {
+	if (addr != nullptr) {
+		socklen_t addrlen = static_cast<socklen_t>(sizeof *addr);
+		return ::accept4(sockFd, reinterpret_cast<struct sockaddr*>(addr), &addrlen, SOCK_NONBLOCK|SOCK_CLOEXEC);
+	} else {
+		return ::accept4(sockFd, nullptr, nullptr, SOCK_NONBLOCK|SOCK_CLOEXEC);
+	}
+}
+
+int Socket::connect(int sockFd, const struct sockaddr_in &addr) {
+	return ::connect(sockFd, reinterpret_cast<const struct sockaddr*>(&addr), static_cast<socklen_t>(sizeof addr));
+}
+
+int Socket::shutdownWrite(int sockFd) {
+	return ::shutdown(sockFd, SHUT_WR);
+}
+
+int Socket::setReuseAddr(int sockFd, int optval) {
+	return ::setsockopt(sockFd, SOL_SOCKET, SO_REUSEADDR, &optval, static_cast<socklen_t>(sizeof optval));
+}
+
+int Socket::setKeepAlive(int sockFd, int optval) {
+	return ::setsockopt(sockFd, SOL_SOCKET, SO_KEEPALIVE, &optval, static_cast<socklen_t>(sizeof optval));
+}
+
+int Socket::getError(int sockFd, int *optval) {
+	if (optval != nullptr) {
+		socklen_t optlen = static_cast<socklen_t>(sizeof *optval);
+		return ::getsockopt(sockFd, SOL_SOCKET, SO_ERROR, optval, &optlen);
+	} else {
+		return ::getsockopt(sockFd, SOL_SOCKET, SO_ERROR, nullptr, nullptr);
+	}
+}
+
+int Socket::getSockName(int sockFd, struct sockaddr_in *addr) {
+	if (addr != nullptr) {
+		socklen_t addrlen = static_cast<socklen_t>(sizeof *addr);
+		return ::getsockname(sockFd, reinterpret_cast<struct sockaddr*>(addr), &addrlen);
+	} else {
+		return ::getsockname(sockFd, nullptr, nullptr);
+	}
+}
+
+int Socket::getPeerName(int sockFd, struct sockaddr_in *addr) {
+	if (addr != nullptr) {
+		socklen_t addrlen = static_cast<socklen_t>(sizeof *addr);
+		return ::getpeername(sockFd, reinterpret_cast<struct sockaddr*>(addr), &addrlen);
+	} else {
+		return ::getpeername(sockFd, nullptr, nullptr);
+	}
+}
---
+++ net/Socket.h
@@ -0,0 +1,31 @@
+#ifndef IAK_NET_SOCKET_H
+#define IAK_NET_SOCKET_H
+
+#include <memory>
+
+#include <arpa/inet.h>
+
+namespace iak {
+namespace net {
+
+class Socket {
+public:
+	static int open();
+	static int close(int sockFd);	
+	static int bind(int sockFd, const struct sockaddr_in &addr);
+	static int listen(int sockFd);
+	static int accept(int sockFd, struct sockaddr_in *addr);
+	static int connect(int sockFd, const struct sockaddr_in &addr);
+	static int shutdownWrite(int sockFd);
+
+	static int setReuseAddr(int sockFd, int optval);
+	static int setKeepAlive(int sockFd, int optval);
+	static int getError(int sockFd, int *optval);
+	static int getSockName(int sockFd, struct sockaddr_in *addr);
+	static int getPeerName(int sockFd, struct sockaddr_in *addr);
+}; // end class Socket
+
+} // end namespace net
+} // end namespace iak
+
+#endif // IAK_NET_SOCKET_H
